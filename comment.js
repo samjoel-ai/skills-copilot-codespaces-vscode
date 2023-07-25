9faee548ba533759cc86b1e9f3e8695c131f8c3a
samjoel-ai <140488419+samjoel-ai@users.noreply.github.com>
2023-07-25 10:15:10 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,64 @@
+//create web server
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/Comment');
+
+// get all comments
+router.get('/', async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// submit a comment
+router.post('/', async (req, res) => {
+    const comment = new Comment({
+        name: req.body.name,
+        comment: req.body.comment,
+        date: req.body.date
+    });
+    try {
+        const savedComment = await comment.save();
+        res.json(savedComment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// get a specific comment
+router.get('/:commentId', async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        res.json(comment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// delete a specific comment
+router.delete('/:commentId', async (req, res) => {
+    try {
+        const removedComment = await Comment.remove({ _id: req.params.commentId });
+        res.json(removedComment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// update a specific comment
+router.patch('/:commentId', async (req, res) => {
+    try {
+        const updatedComment = await Comment.updateOne(
+            { _id: req.params.commentId },
+            { $set: { name: req.body.name, comment: req.body.comment } }
+        );
+        res.json(updatedComment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+module.exports = router;
